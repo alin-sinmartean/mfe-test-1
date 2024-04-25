2af8e8f0e81cb139f67971ff65b29687b6fdafe0
alin-sinmartean <AlinMircea.Sinmartean@cognizant.com>
2024-04-25 09:38:42 +0300

    createGenerateClassnames

--- container/src/App.js
+++ container/src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const createGenerateClassName = createGenerateClassName({
+  productionPrefix: 'co' // putem sa-l punem si ca altceva daca vrem, ideea e sa fie unic.
+  // e practic ca un fel de namespace
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider createGenerateClassName={createGenerateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   )
 }
