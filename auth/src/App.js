b1b4d019d1c9283f59fdd21b3df4f37383a8ca50
alin-sinmartean <AlinMircea.Sinmartean@cognizant.com>
2024-05-18 07:33:49 +0300

    added auth sub app

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import SignUp from './components/Signup';
+import SignIn from './components/Signin';
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma' // pt prod, in loc de prefixul jss, sa fie ma
+  productionPrefix: 'au' // pt prod, in loc de prefixul jss, sa fie au
 });
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
+            <Route path="/auth/signin">
+              <SignIn onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <SignUp onSignIn={onSignIn} />
+            </Route>
+
           </Switch>
         </Router>
       </StylesProvider>
--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -5,17 +5,21 @@ const packageJson = require("../package.json");
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8080/'
+  },
   devServer: {
     port: 8080,
     historyApiFallback: {
-      index: 'index.html'
+      index: '/index.html'
     }
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container', // asta nu e folosit in mod direct, dar e bine sa fie setat
       remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js' // asta e remote-ul nostru, care vine de la marketing
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js', // asta e remote-ul nostru, care vine de la marketing
+        auth: 'auth@http://localhost:8082/remoteEntry.js'
       },
       // shared: ['react', 'react-dom']
       shared: packageJson.dependencies // asa putem pune automat toate versiune de librarii care sunt in package.json. putem sa fim si super specifici in legatura cu versiunile
